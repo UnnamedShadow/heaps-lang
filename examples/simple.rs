f8da3892b9815e1f0eb6fccefd67ea3a72d564cc
Unnamed Shadow <anonymous@example.com>
2024-09-21 17:34:27 +0200

    uses heaps-std

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,24 +1,4 @@
-trait addTrait<T> {
-    type Output;
-    fn add(&self, rhs: T) -> Self::Output;
-}
-impl addTrait<String> for String {
-    type Output = String;
-    fn add(&self, rhs: String) -> Self::Output {
-        format!("{}{}", self, rhs).to_string()
-    }
-}
-
-trait printTrait {
-    type Output;
-    fn print(&self) -> Self::Output;
-}
-impl printTrait for String {
-    type Output = ();
-    fn print(&self) -> Self::Output {
-        println!("{}", self);
-    }
-}
+use heaps_std::*;
 
 heaps_lang::heaps_sync! {
     fa(a b) = {
